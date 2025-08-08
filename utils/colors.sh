a284c53b73a3f0d17e8dad1dbe02388d4eb795dd
MrNova420 <155208275+MrNova420@users.noreply.github.com>
2025-08-07 20:52:34 -0400

    Update colors.sh

--- utils/colors.sh
+++ utils/colors.sh
@@ -1,10 +1,25 @@
-bash
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Color definitions for output
-
+# ----------- Colors -----------
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 RESET='\033[0m'
+
+# ----------- Logging Helpers -----------
+log_info() {
+    echo -e "${BLUE}[INFO]${RESET} $1"
+}
+
+log_success() {
+    echo -e "${GREEN}[OK]${RESET} $1"
+}
+
+log_warn() {
+    echo -e "${YELLOW}[WARN]${RESET} $1"
+}
+
+log_error() {
+    echo -e "${RED}[ERROR]${RESET} $1"
+}
